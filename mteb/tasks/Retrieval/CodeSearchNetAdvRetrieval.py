9b9a2fc46af3af96d50c94c8e6968e7f33fa59ba
Markus Krimmel <markus.krimmel@jina.ai>
2023-12-01 15:26:34 +0100

    feat: improved quality of comment removal function that I copied from StackOverflow

--- mteb/tasks/Retrieval/CodeSearchNetAdvRetrieval.py
+++ mteb/tasks/Retrieval/CodeSearchNetAdvRetrieval.py
@@ -19,7 +19,6 @@ def remove_comments_and_docstrings(source):
         token_string = tok[1]
         start_line, start_col = tok[2]
         end_line, end_col = tok[3]
-        ltext = tok[4]
         if start_line > last_lineno:
             last_col = 0
         if start_col > last_col:
@@ -27,10 +26,8 @@ def remove_comments_and_docstrings(source):
         if token_type == tokenize.COMMENT:
             pass
         elif token_type == tokenize.STRING:
-            if prev_toktype != tokenize.INDENT:
-                if prev_toktype != tokenize.NEWLINE:
-                    if start_col > 0:
-                        out += token_string
+            if prev_toktype != tokenize.INDENT and prev_toktype != tokenize.NEWLINE and start_col > 0:
+                out += token_string
         else:
             out += token_string
         prev_toktype = token_type
